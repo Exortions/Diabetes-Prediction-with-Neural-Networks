c89db29c83058b97effb548a941f06eb39bbf5ad
Itchss <vigiment@gmail.com>
2021-12-14 17:50:02 -0800

    Added visualization

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-from sklearn.model_selection import train_test_split
-from keras.models import Sequential
-from keras.layers import Dense
+import matplotlib
+matplotlib.use("TkAgg")
 from utils import preprocess
 import pandas as pd
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import roc_curve
+from keras.models import Sequential
+from keras.layers import Dense
+import matplotlib.pyplot as plt
+import numpy as np
+
+epochs = 200
 
 model = Sequential()
 
@@ -21,9 +30,35 @@ model.add(Dense(16, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
 
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-model.fit(X_train, y_train, epochs=200)
+
+input(f'Press any key to generate {epochs} epochs. >> ')
+
+model.fit(X_train, y_train, epochs=epochs)
 
 scores = model.evaluate(X_train, y_train)
-print('Training Accuracy: %.2f%%\n' % (scores[1] * 100))
+print('\nTraining Accuracy: %.2f%%\n' % (scores[1] * 100))
 scores = model.evaluate(X_test, y_test)
 print('Testing Accuracy: %.2f%%\n' % (scores[1] * 100))
+
+see_results = input('Do you want to see the visualized results? [Y/N] >> ')
+
+if (see_results == 'y'):
+    y_test_pred = model.predict_classes(X_test)
+    c_matrix = confusion_matrix(y_test, y_test_pred)
+    ax = sns.heatmap(c_matrix, annot=True, xticklabels=['No Diabetes', 'Diabetes'], yticklabels=['No Diabetes', 'Diabetes'], cbar=False, cmap='Blues')
+    ax.set_xlabel("Prediction")
+    ax.set_ylabel("Actual")
+    plt.show()
+    plt.clf()
+
+    y_test_pred_probs = model.predict(X_test)
+    FPR, TPR, _ = roc_curve(y_test, y_test_pred_probs)
+    plt.plot(FPR, TPR)
+    plt.plot([0,1],[0,1],'--', color='black')
+    plt.title('ROC Curve')
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.show()
+    plt.clf()
+
+print('Diabetes predictions finished!')
--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+import time
+
 from sklearn.preprocessing import StandardScaler
 from sklearn import preprocessing
 
@@ -11,12 +13,29 @@ def preprocess(df):
         missing_rows = df.loc[df[col]==0].shape[0]
         print(col + ": " + str(missing_rows))
     print('----------------------------------------------')
+    time.sleep(0.1)
+
+    print('Starting preprocessing...')
+    time.sleep(0.1)
 
     df['Glucose'] = df['Glucose'].fillna(df['Glucose'].mean())
+    print('Glucose pressure preprocessed.')
+    time.sleep(0.1)
     df['BloodPressure'] = df['BloodPressure'].fillna(df['BloodPressure'].mean())
+    print('Blood pressure preprocessed.')
+    time.sleep(0.1)
     df['SkinThickness'] = df['SkinThickness'].fillna(df['SkinThickness'].mean())
+    print('Skin thickness preprocessed.')
+    time.sleep(0.1)
     df['Insulin'] = df['Insulin'].fillna(df['Insulin'].mean())
+    print('Insulin levels preprocessed.')
+    time.sleep(0.1)
     df['BMI'] = df['BMI'].fillna(df['BMI'].mean())
+    print('Body Mass Index preprocessed.')
+    time.sleep(0.1)
+
+    print('Preprocessing finished!')
+    time.sleep(0.1)
 
     print('----------------------------------------------')
     print("After preprocessing")
@@ -25,6 +44,7 @@ def preprocess(df):
         missing_rows = df.loc[df[col] == 0].shape[0]
         print(col + ": " + str(missing_rows))
     print('----------------------------------------------')
+    time.sleep(0.1)
 
     df_scaled = preprocessing.scale(df)
     df_scaled = pd.DataFrame(df_scaled, columns=df.columns)
